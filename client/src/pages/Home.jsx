6e2f4418dc028350d2da09866fd0d756fdb4e373
Mohanraj S <mohanrajatn28@gmail.com>
2025-05-28 10:01:14 +0000

    homepg

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+import { Link } from "react-router";
+export default function Home() {
+  return (
+    <>
+      <section className="min-h-screen flex flex-col items-center justify-center px-4 bg-gradient-to-br from-indigo-100 via-white to-indigo-200">
+        <div className="text-gray-900 text-4xl font-extrabold mb-3 drop-shadow">
+          Expense Tracker
+        </div>
+        <div className="text-gray-600 text-lg mb-8 text-center max-w-md">
+          Take control of your finances! Track your expenses, set budgets, and
+          visualize your spending habits—all in one place.
+        </div>
+        <Link
+          to={'/login'}
+          className="bg-teal-600 hover:bg-teal-700 text-white px-8 py-3 rounded-xl shadow-lg text-lg font-semibold transition duration-300"
+        >
+          Get Started
+        </Link>
+      </section>
+    </>
+  );
+}
